086b28d0877397e29ab25ed91faed97873a9f922
chloemarion <lacymendes98@gmail.com>
2018-08-08 16:06:34 +0000

    automatically backed up by learn

--- loopprac.rb
+++ loopprac.rb
@@ -1,9 +1,19 @@
-def friends
-  num_friends = 0 
-  while num_friends <= 12 
-  puts "I have #{num_friends} frannnnns."
-  num_friends += 1 
-end 
-end 
+# def friends
+#   num_friends = 0 
+#   while num_friends <= 12 
+#   puts "I have #{num_friends} frannnnns."
+#   num_friends += 1 
+# end 
+# end 
 
-friends 
+# friends 
+
+def friends 
+  num_friends = 0 
+  until num_friends == 12
+  puts "I have #{num friends} frannnnnns."
+  num_friends += 1 
+ end 
+end
+
+friends
